6e02800fa71dac797e37a0c81fb2e2be74341dd0
Abner Tellez <atellezsazo@gmail.com>
2017-10-07 02:05:46 +0800

    Delete blank spaces
    Delete blank spaces

--- helpers/csv_constructor.py
+++ helpers/csv_constructor.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 """csv_constructor.py: Download and create csv file for dataset"""
-
 __author__      = "Abner Tellez"
 __copyright__   = "Copyright 2017, NTHU"
 
@@ -18,6 +17,7 @@ DATA_SET_CSV = DATA_SET_DIR + "dataset.csv"
 FILE_NAME = "dataset.zip"
 
 def download_zip(url) :
+    print('Download files...')
     urllib.request.urlretrieve(url, "dataset.zip")
     # check if directory exists
     if not os.path.exists(DATA_SET_DIR):
@@ -35,8 +35,6 @@ def download_zip(url) :
                     shutil.copyfileobj(source, target)
         os.remove("dataset.zip")
         create_csv()
-        
-        
 
 def create_csv():
     with open(DATA_SET_CSV, 'w') as csvfile:
@@ -49,7 +47,3 @@ def create_csv():
                 for line in infile:
                     filewriter.writerow([source] + line.rstrip().split('\t'))
     print('Done!')
- 
-                
-
-
